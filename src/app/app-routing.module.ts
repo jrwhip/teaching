9f210217f70109e180a34b4983040fd8ff550395
Jared Whipple <jared.whipple@stgconsulting.com>
2020-10-10 20:22:15 -0600

    Setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'junk', loadChildren: () => import('./junk/junk.module').then(m => m.JunkModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/junk/junk.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-junk',
+  templateUrl: './junk.component.html',
+  styleUrls: ['./junk.component.scss']
+})
+export class JunkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
