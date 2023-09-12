f6d4a4ac0d5c42040f6a7502e6f38c3ba884a236
jewhipple <jewhipple@gmail.com>
2023-09-11 22:01:49 -0600

    added a rounding question

--- src/app/math/math.component.ts
+++ src/app/math/math.component.ts
@@ -1,23 +1,102 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
-import { mergeMap, map, tap } from 'rxjs/operators';
-
-import { WordService } from '../word.service';
 
 @Component({
   selector: 'app-math',
   templateUrl: './math.component.html',
 })
 export class MathComponent implements OnInit {
-  wordGroups$: Observable<any>;
-  words: string[];
 
-  constructor(private route: ActivatedRoute, private wordService: WordService) { }
+  roundedNumbers: any[] = [];
+  correctStreak: number = 0;
+  roundingFactor: number = 10; // Default rounding factor is tens
+  randomNumber: string = '';
+  userAnswer: number | null = null;
+  result: string = '';
+  resultColor: string = '';
+
+  constructor() { }
 
   ngOnInit(): void {
-    console.log('Hello from MathComponent.ngOnInit()');
+    this.generateNewNumber();
   }
 
+  generateRandomNumber(): string {
+    const min = 10.0; // Minimum number with at least tenths
+    const max = 9999.999; // Maximum number with up to thousandths
+    return this.getRandomFloat(min, max).toFixed(3); // Generate a number with 3 decimal places
+  }
 
+  getRandomFloat(min: number, max: number): number {
+    return Math.random() * (max - min) + min;
+  }
+
+  randomizeRoundingFactor(): void {
+    const roundingOptions = [10, 100, 0.1, 0.01];
+    const randomIndex = Math.floor(Math.random() * roundingOptions.length);
+    this.roundingFactor = roundingOptions[randomIndex];
+    this.generateNewNumber();
+  }
+
+  checkAnswer(userAnswer: number | null): void {
+    const parsedRandomNumber = parseFloat(this.randomNumber);
+
+    if (userAnswer !== null && !isNaN(userAnswer)) {
+      let roundedNumber;
+
+      if (this.roundingFactor === 0.1) {
+        // When rounding to tenths, round to one decimal place
+        roundedNumber = Math.round(parsedRandomNumber * 10) / 10;
+      } else if (this.roundingFactor === 0.01) {
+        // When rounding to hundredths, round to two decimal places
+        roundedNumber = Math.round(parsedRandomNumber * 100) / 100;
+      } else {
+        // For other rounding factors, use the previous method
+        roundedNumber = Math.round(parsedRandomNumber / this.roundingFactor) * this.roundingFactor;
+      }
+
+      const isCorrect = userAnswer === roundedNumber;
+
+      if (isCorrect) {
+        this.result = "Correct!";
+        this.resultColor = "green";
+        this.correctStreak++;
+      } else {
+        this.result = "Incorrect. The correct answer is " + roundedNumber;
+        this.resultColor = "red";
+        this.correctStreak = 0; // Reset streak on incorrect answer
+      }
+
+      // Add the rounded number, original rounding factor, and whether it was correct or incorrect to the list
+      this.roundedNumbers.unshift({ original: parsedRandomNumber, rounded: roundedNumber, roundingFactor: this.roundingFactor, isCorrect });
+
+      // Clear the input box
+      this.userAnswer = null;
+
+      // Randomly assign a new rounding factor
+      this.randomizeRoundingFactor();
+    }
+  }
+
+  generateNewNumber(): void {
+    this.randomNumber = this.generateRandomNumber();
+  }
+
+  changeRoundingFactor(factor: number): void {
+    this.roundingFactor = factor;
+    this.generateNewNumber();
+  }
+
+  getPlaceText(factor: number): string {
+    if (factor === 10) {
+      return "tens";
+    } else if (factor === 100) {
+      return "hundreds";
+    } else if (factor === 0.1) {
+      return "tenths";
+    } else if (factor === 0.01) {
+      return "hundredths";
+    } else {
+      return "unknown";
+    }
+  }
 }
--- src/app/math/math.module.ts
+++ src/app/math/math.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'; // Import ReactiveFormsModule
 
 import { MathComponent } from './math.component';
 
 import { MathRoutingModule } from './math-routing.module';
 
-
 @NgModule({
   declarations: [MathComponent],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     MathRoutingModule
   ]
 })
