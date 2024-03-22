3425b5e56b3325dfb97353761633a642a457e4fa
Jared Whipple <Jared@Whipple.io>
2024-03-22 05:25:38 -0600

    Cleanup problem generation service initialization

--- src/app/services/problem-generation/problem-generation.service.ts
+++ src/app/services/problem-generation/problem-generation.service.ts
@@ -16,18 +16,11 @@ export class ProblemGenerationService {
   private functionMap: { [key: string]: ProblemFn } = {};
   private isInitialized = false;
 
-  // eslint-disable-next-line @typescript-eslint/no-useless-constructor
-  constructor() {
-    // Dynamically import functions from 'foo.ts'
-    // this.initializeFunctionRegistry();
-  }
-
   async initializeFunctionRegistry() {
     if (this.isInitialized) {
       return;
     }
     try {
-      // const module = await import('../foo');
       Object.entries(ProblemCategories).forEach(([categoryName, categoryFunctions]: [string, CategoryFunctions]) => {
         console.log('categoryName:', categoryName);
         console.log('categoryFunctions:', categoryFunctions);
@@ -40,14 +33,6 @@ export class ProblemGenerationService {
     }
     this.isInitialized = true;
   }
-
-  // private initializeFunctionRegistry() {
-  //   Object.entries(ProblemCategories).forEach(([categoryName, categoryFunctions]) => {
-  //     Object.keys(categoryFunctions).forEach(funcName => {
-  //       this.registerFunction(`${categoryName}.${funcName}`, categoryFunctions[funcName]);
-  //     });
-  //   });
-  // }
 
   private registerFunction(key: string, func: ProblemFn) {
     this.functionMap[key] = func;
