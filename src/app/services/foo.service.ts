ef540bb2a3a6bbb73f955b1e0c048c70a37a86f4
bjwhip <31357095+bjwhip@users.noreply.github.com>
2024-03-22 17:29:26 -0700

    counter value method not workingn

--- src/app/services/foo.service.ts
+++ src/app/services/foo.service.ts
@@ -21,6 +21,8 @@ export class FooService {
     return this.stateService.selectKey('storedMathQuestions');
   }
 
+  //Take 1 or infinite loop
+
   // TODO: This tries to store in state the validate function, but my state service is not setup to
   // handle storing functions.
   // This needs to be brainstormed more. Because as it is the way Jim wants to do it will not work.
@@ -31,11 +33,13 @@ export class FooService {
   }
 
   updateCounterData(counterData: CounterData) {
+    console.log('Here is that counter Data: ', counterData);
     this.stateService.patchState({ counterData });
   }
 
   setNewMathQuestion(operation: string) {
-    const capitalizeFirstLetter = (str: string) => str.charAt(0).toUpperCase() + str.slice(1);
+    const capitalizeFirstLetter = (str: string) =>
+      str.charAt(0).toUpperCase() + str.slice(1);
     // TODO: This is a hacky way to get the function name. This should be refactored.
     // The function name needs to be listed in math-operations.ts.
     const foo = `Basics.generate${capitalizeFirstLetter(operation)}Problem`;
@@ -58,6 +62,57 @@ export class FooService {
           [operation]: newMathQuestion,
         };
         this.setStoredMathQuestions(newStoredMathQuestions);
+      })
+    );
+  }
+
+  setNewCounterValues(operation: string, isCorrect: boolean) {
+    return this.stateService.counterData$.pipe(
+      take(1),
+      map((currentCounterData) => {
+        const currentOperationCount = (currentCounterData
+          ? currentCounterData[operation]
+          : undefined) || {
+          correct: 0,
+          incorrect: 0,
+          streak: 0,
+          highStreak: 0,
+          label: operation,
+        };
+
+        // Update correct or incorrect count based on isCorrect
+        const updatedCorrect = isCorrect
+          ? currentOperationCount.correct + 1
+          : currentOperationCount.correct;
+        const updatedIncorrect = !isCorrect
+          ? currentOperationCount.incorrect + 1
+          : currentOperationCount.incorrect;
+
+        // Update streak and potentially highStreak if answer is correct
+        let updatedStreak = currentOperationCount.streak;
+        let updatedHighStreak = currentOperationCount.highStreak;
+
+        if (isCorrect) {
+          updatedStreak += 1; // Increment streak if correct
+          if (updatedStreak > updatedHighStreak) {
+            updatedHighStreak = updatedStreak; // Update highStreak if current streak is greater
+          }
+        } else {
+          updatedStreak = 0; // Reset streak if incorrect
+        }
+
+        const newCounterData = {
+          ...currentCounterData,
+          [operation]: {
+            correct: updatedCorrect,
+            incorrect: updatedIncorrect,
+            streak: updatedStreak,
+            highStreak: updatedHighStreak,
+            label: operation,
+          },
+        };
+
+        this.updateCounterData(newCounterData);
       })
     );
   }
